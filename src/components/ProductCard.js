d6b4546ef0582a5be62ecca3c2d2e5977b75f97d
SultanKhassenov <154452152+SultanKhassenov@users.noreply.github.com>
2023-12-23 04:10:19 +0600

    ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,52 +1,34 @@
 import React, { useState } from "react";
 
-function ProductCard({ products, addItem }) {
-  const handleAddItem = (id, name, price, urlimage, url) => {
-    const newItem = { id, name, price, url, urlimage };
-    onAdditem3(newItem);
-    console.log(newItem);
+function ProductCard({ products, addProduct }) {
+  const handleAddProduct = (id, name, price, image, url) => {
+    const newProduct = { id, name, price, url, image };
+    onAddProduct3(newProduct);
   };
-  const onAdditem3 = (Item) => {
-    addItem(Item);
-  };
+  const onAddProduct3 = addProduct;
+
 
   return (
     <div className="products-container">
-      {products.map((product) => (
-        <div key={product.id} className="product-card">
-          <p className="product-prices">
-            ● ${product.price}
-          </p>
-          <div className="productImageFrame">
-            <img
-              src={product.urlimage}
-              alt={product.name}
-              className="product-image"
-            />
-          </div>
-          <div className="product-details">
-            <a href={product.url}>
-              <p>{product.name}</p>
-            </a>
-            <button
-            className="addToCart"
-            onClick={() =>
-              handleAddItem(
-                product.id,
-                product.name,
-                product.price,
-                product.urlimage,
-                product.url
-              )
-            }
-            style={{ cursor: "pointer" }}
-          >+</button>
-          </div>
-          
-          
-        </div>
-      ))}
+        {products.map((product) => (
+            <div key={product.id} className="product-card">
+                <p className="product-prices">${product.price} </p>
+                <div className="productImageFrame">
+                    <img src={product.image} alt={product.name} className="product-image" />
+                </div>
+                <div className="product-details">
+                    <a href={product.url}>
+                        <p>{product.name}</p>
+                    </a>
+                    <button className="addToCart" onClick={() =>
+                        handleAddProduct(product.id, product.name, product.price, product.image, product.url)}
+                        style={{ cursor: "pointer" }}
+                    >+</button>
+                </div>
+            </div>
+        ))}
     </div>
   );
 }
 export default ProductCard;
+
