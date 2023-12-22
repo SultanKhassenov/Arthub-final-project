09be127d882da1808b80dcb21ce09d1fc1f3edce
SultanKhassenov <154452152+SultanKhassenov@users.noreply.github.com>
2023-12-23 04:14:45 +0600

    App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,49 @@ import Store from "./components/Store";
 import MyNavbar from "./components/MyNavbar";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import UserProfile from "./components/UserProfile";
+import RegistrationForm from "./components/Registration";
+import LoginForm from "./components/Login";
 
 function App() {
   const [items, setItems] = useState([]);
-  const addItem = (Item) => {
+  const [user, setUser] = useState(null);
+
+  const addProduct = (Product) => {
     const newItem = {
-      id: Item.id,
-      name: Item.name,
-      price: Item.price,
-      urlimage: Item.urlimage,
-      url: Item.url,
+      id: Product.id,
+      name: Product.name,
+      price: Product.price,
+      urlimage: Product.urlimage,
+      url: Product.url,
     };
 
     setItems([...items, newItem]);
   };
 
+  const handleRegister = (newUser) => {
+    setUser(newUser);
+  };
+
+  const handleLogin = (user) => {
+    setUser(user);
+  };
+
   return (
-    <BrowserRouter>
-      <MyNavbar />
+      <BrowserRouter>
+        <MyNavbar />
 
-      <Routes>
-        <Route path="/cart" element={<Cart products={items} />} />
-        <Route path="/store" element={<Store addItem={addItem} />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-
-    </BrowserRouter>
+        <Routes>
+          <Route path="/cart" element={<Cart products={items} />} />
+          <Route path="/store" element={<Store addProduct={addProduct} />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="/userProfile" element={<UserProfile user={user} />} />
+          <Route path="/register" element={<RegistrationForm onRegister={handleRegister} onLogin={handleLogin} />} />
+          <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
